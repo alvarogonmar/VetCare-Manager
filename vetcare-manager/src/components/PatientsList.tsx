47cb13586ef6db83739c6786e24233450e57ff7d
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 19:36:55 -0600

    Add conditional rendering in PatientsList to display message when no patients are present

--- vetcare-manager/src/components/PatientsList.tsx
+++ vetcare-manager/src/components/PatientsList.tsx
@@ -2,5 +2,21 @@ import { usePatientStore } from "../store/store";
 
 export default function PatientsList() {
   const patients = usePatientStore((state) => state.patients);
-  return <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll"></div>;
+  return (
+    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+      {patients.length ? (
+        <p>yes</p>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No patients</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Start adding patients {""}
+            <span className="text-indigo-600 font-bold">
+              and they will appear here
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
