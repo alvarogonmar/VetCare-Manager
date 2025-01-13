baa51fe054817fba1f40b1fcadddd67572d47a66
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 19:30:42 -0600

    Integrate patient store into PatientsList component to display patient data

--- vetcare-manager/src/components/PatientsList.tsx
+++ vetcare-manager/src/components/PatientsList.tsx
@@ -1,3 +1,6 @@
+import { usePatientStore } from "../store/store";
+
 export default function PatientsList() {
-  return <div>PatientsList</div>;
+  const patients = usePatientStore((state) => state.patients);
+  return <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll"></div>;
 }
