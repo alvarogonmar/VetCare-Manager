28d1a78e1b9d31a0b3e21c943b92ce37955e2027
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 18:01:13 -0600

    Enhance App component header with improved styling and layout

--- vetcare-manager/src/App.tsx
+++ vetcare-manager/src/App.tsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl">Patients</h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          VetCare {""}
+          <span className="text-indigo-700">Manager</span>
+        </h1>
+      </div>
     </>
   );
 }
