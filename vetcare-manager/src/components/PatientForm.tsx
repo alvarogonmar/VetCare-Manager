9c608c7e1d334740f6a4768c6aa373235416bf90
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 18:27:15 -0600

    Add form submission handling and error display in PatientForm component

--- vetcare-manager/src/components/PatientForm.tsx
+++ vetcare-manager/src/components/PatientForm.tsx
@@ -1,7 +1,13 @@
 import { useForm } from "react-hook-form";
 
 export default function PatientForm() {
-  const { register } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const registerPatient = () => {};
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Patient Tracking</h2>
@@ -14,6 +20,7 @@ export default function PatientForm() {
       <form
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
         noValidate
+        onSubmit={handleSubmit(registerPatient)}
       >
         <div className="mb-5">
           <label htmlFor="name" className="text-sm uppercase font-bold">
@@ -28,6 +35,8 @@ export default function PatientForm() {
               required: "Patient name is required",
             })}
           />
+
+          {errors.name?.message}
         </div>
 
         <div className="mb-5">
