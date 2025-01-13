807dec7f897dd4d9d3a909cc1c1c04eca7327357
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 21:26:11 -0600

    Add delete functionality to PatientDetails component

--- vetcare-manager/src/store/store.ts
+++ vetcare-manager/src/store/store.ts
@@ -5,6 +5,7 @@ import { DraftPatient, Patient } from "../types"
 type PatientState = {
     patients: Patient[]
     addPatient: (data: DraftPatient) => void
+    deletePatient: (id: Patient["id"]) => void
 }
 
 const createPatient = (patient: DraftPatient) : Patient => {
@@ -17,6 +18,11 @@ export const usePatientStore = create<PatientState>((set) => ({
         const newPatient = createPatient(data)
         set((state) => ({ // SET ES COMO EL RETURN, pero es el que se utiliza con zustand, setear informacion
 patients: [...state.patients, newPatient]
+        }))
+    },
+    deletePatient: (id) => {
+        set((state) => ({
+            patients: state.patients.filter(patient => patient.id !== id)
         }))
     }
 }))
