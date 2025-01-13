12114d8c9edb665f390b4e7ad14d03f8e0503a6c
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 21:16:33 -0600

    Add Edit and Delete buttons to PatientDetails component

--- vetcare-manager/src/components/PatientDetails.tsx
+++ vetcare-manager/src/components/PatientDetails.tsx
@@ -14,6 +14,22 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
       <PatientDetailsItem label="Email" data={patient.email} />
       <PatientDetailsItem label="Date" data={patient.date.toString()} />
       <PatientDetailsItem label="Symptoms" data={patient.symptoms} />
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        >
+          Edit
+        </button>
+
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
