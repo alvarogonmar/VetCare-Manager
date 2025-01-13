a92ee40aa72c5e0b2c1a2f99da4644b17564d087
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 19:57:32 -0600

    Add structured patient information display in PatientDetails component

--- vetcare-manager/src/components/PatientDetails.tsx
+++ vetcare-manager/src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import { Patient } from "../types";
+import PatientDetailsItem from "./PatientDetailsItem";
 
 type PatientDetailsProps = {
   patient: Patient;
@@ -6,6 +7,13 @@ type PatientDetailsProps = {
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
   return (
-    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl"></div>
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+      <PatientDetailsItem label="ID" data={patient.id} />
+      <PatientDetailsItem label="Name" data={patient.name} />
+      <PatientDetailsItem label="Caretaker" data={patient.caretaker} />
+      <PatientDetailsItem label="Email" data={patient.email} />
+      <PatientDetailsItem label="Date" data={patient.date.toString()} />
+      <PatientDetailsItem label="Symptoms" data={patient.symptoms} />
+    </div>
   );
 }
