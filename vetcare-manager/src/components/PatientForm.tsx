262c8364ff05b1ff5cce6b919aa70ea390f9fb5c
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 18:09:38 -0600

    Enhance PatientForm component with structured layout and input fields for patient details

--- vetcare-manager/src/components/PatientForm.tsx
+++ vetcare-manager/src/components/PatientForm.tsx
@@ -1,3 +1,81 @@
 export default function PatientForm() {
-  return <div>PatientForm</div>;
+  return (
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+      <h2 className="font-black text-3xl text-center">Patient Tracking</h2>
+
+      <p className="text-lg mt-5 text-center mb-10">
+        Add Patients and {""}
+        <span className="text-indigo-600 font-bold">Manage Them</span>
+      </p>
+
+      <form
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        noValidate
+      >
+        <div className="mb-5">
+          <label htmlFor="name" className="text-sm uppercase font-bold">
+            Patient
+          </label>
+          <input
+            id="name"
+            className="w-full p-3 border border-gray-100"
+            type="text"
+            placeholder="Patient's Name"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="caretaker" className="text-sm uppercase font-bold">
+            Caretaker
+          </label>
+          <input
+            id="caretaker"
+            className="w-full p-3 border border-gray-100"
+            type="text"
+            placeholder="Caretaker's Name"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="email" className="text-sm uppercase font-bold">
+            Email
+          </label>
+          <input
+            id="email"
+            className="w-full p-3 border border-gray-100"
+            type="email"
+            placeholder="Registration Email"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="date" className="text-sm uppercase font-bold">
+            Admission Date
+          </label>
+          <input
+            id="date"
+            className="w-full p-3 border border-gray-100"
+            type="date"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="symptoms" className="text-sm uppercase font-bold">
+            Symptoms
+          </label>
+          <textarea
+            id="symptoms"
+            className="w-full p-3 border border-gray-100"
+            placeholder="Patient's Symptoms"
+          ></textarea>
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+          value="Save Patient"
+        />
+      </form>
+    </div>
+  );
 }
