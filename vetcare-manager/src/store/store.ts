e0556bf31481d2d59141ab5e0ce4855ad21ed7e3
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-12 21:58:36 -0600

    Enhance patient store with Zustand devtools and update getPatientById functionality

--- vetcare-manager/src/store/store.ts
+++ vetcare-manager/src/store/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand"
+import { devtools } from "zustand/middleware"
 import { v4 as uuidv4 } from "uuid"
 import { DraftPatient, Patient } from "../types"
 
@@ -14,21 +15,24 @@ const createPatient = (patient: DraftPatient) : Patient => {
      return {...patient, id: uuidv4() } // CREAR ID
 }
 
-export const usePatientStore = create<PatientState>((set) => ({
-    patients: [],
-    activeID: "",
-    addPatient: (data) => {
-        const newPatient = createPatient(data)
-        set((state) => ({ // SET ES COMO EL RETURN, pero es el que se utiliza con zustand, setear informacion
-patients: [...state.patients, newPatient]
-        }))
-    },
-    deletePatient: (id) => {
-        set((state) => ({
-            patients: state.patients.filter(patient => patient.id !== id)
-        }))
-    },
-    getPatientById: (id) => {
-        console.log(id)
-    }
-}))
+export const usePatientStore = create<PatientState>()(devtools((set) => ({
+        patients: [],
+        activeID: "",
+        addPatient: (data) => {
+            const newPatient = createPatient(data)
+            set((state) => ({ // SET ES COMO EL RETURN, pero es el que se utiliza con zustand, setear informacion
+    patients: [...state.patients, newPatient]
+            }))
+        },
+        deletePatient: (id) => {
+            set((state) => ({
+                patients: state.patients.filter(patient => patient.id !== id)
+            }))
+        },
+        getPatientById: (id) => {
+            set(() => ({
+                activeID: id
+            }))
+        }
+    })
+))
