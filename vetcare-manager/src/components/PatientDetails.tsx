3c7df51adc849c4799f6b2344e9ee977cf581f6c
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-13 19:01:28 -0600

    Refactor delete button handler in PatientDetails to improve readability

--- vetcare-manager/src/components/PatientDetails.tsx
+++ vetcare-manager/src/components/PatientDetails.tsx
@@ -9,6 +9,9 @@ type PatientDetailsProps = {
 export default function PatientDetails({ patient }: PatientDetailsProps) {
   const deletePatient = usePatientStore((state) => state.deletePatient);
   const getPatientById = usePatientStore((state) => state.getPatientById);
+  const handleClick = () => {
+    deletePatient(patient.id);
+  };
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailsItem label="ID" data={patient.id} />
@@ -30,7 +33,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-          onClick={() => deletePatient(patient.id)}
+          onClick={handleClick}
         >
           Delete
         </button>
